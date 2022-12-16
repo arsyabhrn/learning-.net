efb576991bfedad88a57ba1550a0b9dab66379a6
arsyabhrn <alfatehanarsya8998@gmail.com>
2022-12-17 06:17:04 +0700

    add controller

---
+++ my-books/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
